76a42612bdc8fe3e9166117157e06c19ad8e162b
HasanYonden <yondenhasan@gmail.com>
2022-08-24 19:30:22 +0200

    Initial commit

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "login-button")
+    public WebElement loginBtn;
+
+    @FindBy(className = "account")
+    public WebElement successMessage;
+
+    @FindBy(className = "validation-summary-errors")
+    public WebElement errorMessage;
+}
